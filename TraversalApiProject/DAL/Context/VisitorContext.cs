dc5c71db2b50c1b7a78edcf85aa019e0bece95a3
Ömer Kocabay <passanger54@hotmail.com>
2024-02-19 18:06:50 +0300

    Add project files.

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-39CPBHK\\KOCABAY; initial catalog= TraversalDbApi; integrated security=true; TrustServerCertificate=true;");
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
